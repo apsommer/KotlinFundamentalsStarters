0edeb4e4259a37083bec59ce4f36b6e0df94c346
Drew Sommer <andrew.sommer@tiaa.org>
2021-07-23 16:27:53 -0500

    refactor: Move logic from controller to viewmodel

--- GuessTheWord-Starter/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ GuessTheWord-Starter/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -5,8 +5,67 @@ import androidx.lifecycle.ViewModel
 
 class GameViewModel: ViewModel() {
 
+    // dependencies
+    var word = ""
+    var score = 0
+    private lateinit var wordList: MutableList<String>
+
     init {
         Log.i("GameViewModel", "GameViewModel has been created!")
+        resetList()
+        nextWord()
+    }
+
+    /**
+     * Resets the list of words and randomizes the order
+     */
+    private fun resetList() {
+
+        wordList = mutableListOf(
+                "queen",
+                "hospital",
+                "basketball",
+                "cat",
+                "change",
+                "snail",
+                "soup",
+                "calendar",
+                "sad",
+                "desk",
+                "guitar",
+                "home",
+                "railway",
+                "zebra",
+                "jelly",
+                "car",
+                "crow",
+                "trade",
+                "bag",
+                "roll",
+                "bubble"
+        )
+        wordList.shuffle()
+    }
+
+    /**
+     * Moves to the next word in the list
+     */
+    private fun nextWord() {
+
+        if (!wordList.isEmpty()) {
+            //Select and remove a word from the list
+            word = wordList.removeAt(0)
+        }
+    }
+
+    /** Methods for buttons presses **/
+    fun onSkip() {
+        score--
+        nextWord()
+    }
+    fun onCorrect() {
+        score++
+        nextWord()
     }
 
     override fun onCleared() {
